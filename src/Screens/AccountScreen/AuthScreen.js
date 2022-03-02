cf4fea50ad25f76ed1644b4c9e17a832e4559e4b
Halil <halil.seran.1135@gmail.com>
2022-03-02 23:30:20 +0300

    AuthScreen

--- src/Screens/AccountScreen/AuthScreen.js
+++ src/Screens/AccountScreen/AuthScreen.js
@@ -1,13 +1,53 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { TextInput } from "react-native-paper";
 
 export const AuthScreen = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>
-                Auth Screen HERE
-            </Text>
-            <Button title="Register" onPress={() => navigation.navigate('RegisterScreen')} />
+  return (
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <Text>Mail or Phone Number</Text>
+        <TextInput
+          style={styles.inputContainer}
+          mode="outlined"
+          label="Mail or Phone Number"
+          placeholder="Type something"
+          right={<TextInput.Affix text="/100" />}
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={styles.inputContainer}
+          mode="outlined"
+          label="Password"
+          placeholder="Type something"
+          right={<TextInput.Affix text="/100" />}
+        />
+        <View style={{flex:1,flexDirection:"row",justifyContent:"space-around", width:"100%"}} >
+          <Button
+            title="Register"
+            onPress={() => navigation.navigate("RegisterScreen")}
+          />
+          <Button title="Login" onPress={() => null} />
         </View>
-    )
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    backgroundColor: "red",
+    width: "70%",
+    height: "20%",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "95%",
+    height: 45,
+  },
+});
