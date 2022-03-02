9019db55fbcd5e5dfca68c0ff54d5ab9f49b80a1
Halil <halil.seran.1135@gmail.com>
2022-03-02 23:39:56 +0300

    RegisterScreen

--- src/Screens/AccountScreen/RegisterScreen.js
+++ src/Screens/AccountScreen/RegisterScreen.js
@@ -2,11 +2,25 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 
 export const RegisterScreen = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>
-                Register Screen HERE
-            </Text>
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Register Screen HERE</Text>
+      <Text>Mail or Phone Number</Text>
+      <TextInput
+        style={styles.inputContainer}
+        mode="outlined"
+        label="Mail or Phone Number"
+        placeholder="Type something"
+        right={<TextInput.Affix text="/100" />}
+      />
+      <Text>Password</Text>
+      <TextInput
+        style={styles.inputContainer}
+        mode="outlined"
+        label="Password"
+        placeholder="Type something"
+        right={<TextInput.Affix text="/100" />}
+      />
+    </View>
+  );
 };
